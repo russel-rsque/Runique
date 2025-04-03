9d53ea431ed39b721e3fe9e10f4acc8ba862aab5
russelr <russel.rosique01@gmail.com>
2025-04-02 20:16:01 -0600

    Implement permission handling in active run screen

--- run/presentation/src/main/java/com/rosique/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/rosique/run/presentation/active_run/ActiveRunScreen.kt
@@ -2,6 +2,8 @@
 
 package com.rosique.run.presentation.active_run
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +25,19 @@ import com.rosique.core.presentation.designsystem.components.RuniqueToolbar
 import org.koin.androidx.compose.koinViewModel
 import com.rosique.run.presentation.R
 import com.rosique.run.presentation.active_run.components.RunDataCard
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import com.rosique.core.presentation.designsystem.components.RuniqueDialog
+import com.rosique.core.presentation.designsystem.components.RuniqueOutlinedActionButton
+import com.rosique.run.presentation.util.hasLocationPermission
+import com.rosique.run.presentation.util.hasNotificationPermission
+import com.rosique.run.presentation.util.shouldShowLocationPermissionRationale
+import com.rosique.run.presentation.util.shouldShowNotificationPermissionRationale
 
 @Composable
 
@@ -41,6 +56,58 @@ private fun ActiveRunScreen(
     state: ActiveRunState,
     onAction: (ActiveRunAction) -> Unit
 ) {
+    val context = LocalContext.current
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { perms ->
+        val hasCoarseLocationPermission = perms[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        val hasFineLocationPermission = perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+        val hasNotificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+            perms[Manifest.permission.POST_NOTIFICATIONS] == true
+        } else true
+
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = hasCoarseLocationPermission && hasFineLocationPermission,
+                showLocationRationale = showLocationRationale
+            )
+        )
+
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = hasNotificationPermission,
+                showNotificationRationale = showNotificationRationale
+            )
+        )
+    }
+
+    LaunchedEffect(Unit) {
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = context.hasLocationPermission(),
+                showLocationRationale = showLocationRationale
+            )
+        )
+
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = context.hasNotificationPermission(),
+                showNotificationRationale = showNotificationRationale
+            )
+        )
+
+        if (!showLocationRationale && !showNotificationRationale) {
+            permissionLauncher.requestRuniquePermissions(context)
+        }
+    }
 
     RuniqueScaffold(
         withGradient = false,
@@ -85,6 +152,62 @@ private fun ActiveRunScreen(
                     .padding(innerPadding)
                     .fillMaxWidth()
             )
+        }
+    }
+
+    if (state.showLocationRationale || state.showNotificationRationale) {
+        RuniqueDialog(
+            title = stringResource(R.string.permission_required),
+            onDismiss = { /* Normal dismissing not allowed for permissions */ },
+            description = when {
+                state.showLocationRationale && state.showNotificationRationale -> {
+                    stringResource(R.string.location_notification_rationale)
+                }
+                state.showLocationRationale -> {
+                    stringResource(R.string.location_rationale)
+                }
+                else -> {
+                    stringResource(R.string.notification_rationale)
+                }
+            },
+            primaryButton = {
+                RuniqueOutlinedActionButton(
+                    text = stringResource(R.string.okay),
+                    isLoading = false,
+                    onClick = {
+                        onAction(ActiveRunAction.DismissRationaleDialog)
+                        permissionLauncher.requestRuniquePermissions(context)
+                    }
+                )
+            }
+        )
+    }
+}
+
+private fun ActivityResultLauncher<Array<String>>.requestRuniquePermissions(
+    context: Context
+) {
+    val hasLocationPermission = context.hasLocationPermission()
+    val hasNotificationPermission = context.hasNotificationPermission()
+
+    val locationPermissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    val notificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+        arrayOf(Manifest.permission.POST_NOTIFICATIONS)
+    } else arrayOf()
+
+    when {
+        !hasLocationPermission && !hasNotificationPermission -> {
+            launch(locationPermissions + notificationPermission)
+        }
+        !hasLocationPermission -> {
+            launch(locationPermissions)
+        }
+        !hasNotificationPermission -> {
+            launch(notificationPermission)
         }
     }
 }
--- run/presentation/src/main/java/com/rosique/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/com/rosique/run/presentation/active_run/ActiveRunViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 
 class ActiveRunViewModel: ViewModel() {
@@ -15,7 +16,44 @@ class ActiveRunViewModel: ViewModel() {
     private val eventChannel = Channel<ActiveRunEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    fun onAction(action: ActiveRunAction) {
+    private val _hasLocationPermission = MutableStateFlow(false)
 
+
+    fun onAction(action: ActiveRunAction) {
+        when(action) {
+            ActiveRunAction.OnBackClick -> {
+
+            }
+
+            ActiveRunAction.OnFinishRunClick -> {
+
+            }
+            ActiveRunAction.OnResumeRunClick -> {
+
+            }
+            ActiveRunAction.OnToggleRunClick -> {
+
+            }
+            is ActiveRunAction.SubmitLocationPermissionInfo -> {
+                _hasLocationPermission.value = action.acceptedLocationPermission
+                state = state.copy(
+                    showLocationRationale = action.showLocationRationale
+                )
+            }
+            is ActiveRunAction.SubmitNotificationPermissionInfo -> {
+                state = state.copy(
+                    showNotificationRationale = action.showNotificationRationale
+                )
+            }
+
+            ActiveRunAction.DismissRationaleDialog -> {
+                state = state.copy(
+                    showNotificationRationale = false,
+                    showLocationRationale = false
+                )
+            }
+
+            else -> Unit
+        }
     }
 }
